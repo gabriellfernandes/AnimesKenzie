acdd7b4c3210d5267c9172e0e843679d1eedc637
Gabriel <gabriellf.dev@gmail.com>
2022-06-23 16:54:34 -0300

    tentando arruma os bugs!

--- js/js-Episodio/episodio.js
+++ js/js-Episodio/episodio.js
@@ -3,12 +3,9 @@ import { animeDetailsHtml, animeEpisodio } from "../buttonSide.js";
 import { buttonSever } from "./buttonServ.js";
 
 
-if(localStorage.getItem('anime') == "tate-no-yuusha-no-nariagari-season-2"){
-    localStorage.setItem('anime', "tate-no-yuusha-no-nariagari-2nd-season")
-    Api.getEpisodiosWatch("tate-no-yuusha-no-nariagari-2nd-season")
-}else{
-    Api.getEpisodiosWatch(localStorage.getItem("episodio"))
-}
+
+Api.getEpisodiosWatch(localStorage.getItem("episodio"))
+
 
 animeDetailsHtml()
 animeEpisodio()
