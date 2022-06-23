6ed5d46be543051b926fe1c8070be90212fae290
Gabriel <gabriellf.dev@gmail.com>
2022-06-23 16:37:03 -0300

    arrumei o tate no yusha

--- js/js-AnimeDetails/anime.js
+++ js/js-AnimeDetails/anime.js
@@ -1,5 +1,12 @@
 import { animeDetailsHtml, animeEpisodio,  animeRecente} from "../buttonSide.js";
 import { Api } from "../api.js";
+if(localStorage.getItem('anime') == "tate-no-yuusha-no-nariagari-season-2"){
+    localStorage.setItem('anime', "tate-no-yuusha-no-nariagari-2nd-season")
+    Api.getDetails("tate-no-yuusha-no-nariagari-2nd-season")
+}else{
+    Api.getDetails(localStorage.getItem("anime"))
+}
+
 Api.getDetails(localStorage.getItem("anime"));
 animeDetailsHtml()
 animeEpisodio()
